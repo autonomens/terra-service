7ee7b7c7ef4d8cdc2958364d9388227e958aa763
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-04-09 10:45:14 +0200

    Adds townships viewset

--- src/tr_geoinsee/urls.py
+++ src/tr_geoinsee/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import StateViewSet, CountyViewSet
+from .views import StateViewSet, CountyViewSet, TownshipViewset
 
 router = routers.SimpleRouter()
 
 router.register(r'state', StateViewSet, base_name='state')
 router.register(r'county', CountyViewSet, base_name='county')
+router.register(r'township', TownshipViewset, base_name='township')
 
 
 urlpatterns = []
