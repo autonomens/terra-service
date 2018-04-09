2e0cd2fa5affc71df277f62028f7d1c12d8d44f1
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-04-09 10:44:49 +0200

    Fix all SparQL queries

--- src/tr_geoinsee/views.py
+++ src/tr_geoinsee/views.py
@@ -1,59 +1,87 @@
 # coding: utf8
 from rest_framework.decorators import detail_route
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 from django.conf import settings
+from django.utils.functional import cached_property
+from django.core.paginator import Paginator
 
-from .serializers import StateSerializer
+from .serializers import StateSerializer, SparQLSerializer
 
 class SparQLViewSet(viewsets.ViewSet):
-    
+
     def get_detail_query(self, pk=None):
         raise NotImplementedError("Please Implement this method")
 
     def get_list_query(self):
         raise NotImplementedError("Please Implement this method")
 
-    def sparql_query(self, query):
+    def sparql_query(self, query, many=False):
         sparql = SPARQLWrapper(settings.INSEE_SPARQL)
         sparql.setQuery(query)
         sparql.setReturnFormat(JSON)
         raw_result = sparql.query()
         results = raw_result.convert()
 
-        response = []
-        vars = results.get('head', {}).get('vars', [])
-        for item in results.get('results', {}).get('bindings', {}):
-            try:
-                response.append({ var: item.get(var).get('value') for var in vars })
-            except Exception:
-                pass
-        return response
+        fields = self.get_query_fields(results)
 
+        if many:
+            resultlist = {}
+            for item in results.get('results', {}).get('bindings', {}):
+                resultitem = resultlist.setdefault(item.get('identifier').get('value'), dict())
+                resultitem.update(self.get_fields_values(fields, item))
+            
+            serializer = SparQLSerializer(list(resultlist.values()), many=True)
+            return serializer.data
+
+        result = {}
+        for item in results.get('results', {}).get('bindings', {}):
+            result.update(self.get_single_item_values(item))
+
+        serializer = SparQLSerializer(result)
         
+        return serializer.data
+    
+    def get_query_fields(self, query):
+        fields = query.get('head', {}).get('vars', [])
+        if 'identifier' in fields:
+            fields.remove('identifier')
+        return fields
+    
+    def get_single_item_values(self, item):
+        prop =  item.get('prop').get('value')
+        return { prop[prop.rfind('#') + 1:]: item.get('value').get('value') }
+
+
+    def get_fields_values(self, fields, item):
+        return { field: item.get(field).get('value') for field in fields }
 
     def list(self, request):
-        return Response(self.sparql_query(self.get_list_query()))
+        return Response(self.sparql_query(self.get_list_query(), True))
 
     def retrieve(self, request, pk=None):
         return Response(self.sparql_query(self.get_detail_query(pk)))
+
 
 class StateViewSet(SparQLViewSet):
 
     def get_detail_query(self, pk=None):
         query = '''
             PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX rdfschema:<http://www.w3.org/2000/01/rdf-schema#>
             PREFIX igeo:<http://rdf.insee.fr/def/geo#>
-            SELECT ?label ?value WHERE {{
+            SELECT ?prop ?value WHERE {{
                     ?state rdf:type igeo:Region .
                     ?state igeo:nom ?name .
                     ?state igeo:vivant true .
                     ?state igeo:codeINSEE ?insee_code .
-                    ?state ?prop ?value .
-                    ?prop <http://www.w3.org/2000/01/rdf-schema#label> ?label
+                    ?prop rdfschema:label ?label .
+                    ?state ?prop ?value
                     FILTER( "{}" = STR(?insee_code)) .
+                    FILTER( isLiteral(?value) )
             }}
             '''
         return query.format(pk)
@@ -62,11 +90,11 @@ class StateViewSet(SparQLViewSet):
         return '''
             PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
             PREFIX igeo:<http://rdf.insee.fr/def/geo#>
-            SELECT ?name ?insee_code WHERE {
-                ?state rdf:type igeo:Region .
-                    ?state igeo:nom ?name .
-                    ?state igeo:vivant true .
-                    ?state igeo:codeINSEE ?insee_code .
+            SELECT ?identifier ?name ?insee_code WHERE {
+                    ?identifier rdf:type igeo:Region .
+                    ?identifier igeo:nom ?name .
+                    ?identifier igeo:vivant true .
+                    ?identifier igeo:codeINSEE ?insee_code .
             }
             '''
         
@@ -74,11 +102,11 @@ class StateViewSet(SparQLViewSet):
         query = '''
             PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
             PREFIX igeo:<http://rdf.insee.fr/def/geo#>
-            SELECT ?code_insee ?name WHERE {{
-                    ?county_id <http://rdf.insee.fr/def/geo#subdivisionDe> ?state .
-                    ?county_id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://rdf.insee.fr/def/geo#Departement> .
-                    ?county_id <http://rdf.insee.fr/def/geo#nom> ?name .
-                    ?county_id <http://rdf.insee.fr/def/geo#codeINSEE> ?code_insee .
+            SELECT ?identifier ?name ?insee_code WHERE {{
+                    ?identifier <http://rdf.insee.fr/def/geo#subdivisionDe> ?state .
+                    ?identifier <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://rdf.insee.fr/def/geo#Departement> .
+                    ?identifier <http://rdf.insee.fr/def/geo#nom> ?name .
+                    ?identifier <http://rdf.insee.fr/def/geo#codeINSEE> ?insee_code .
                     ?state igeo:codeINSEE ?state_insee_code
                     FILTER( "{}" = STR(?state_insee_code))
             }}
@@ -87,22 +115,24 @@ class StateViewSet(SparQLViewSet):
 
     @detail_route(url_path='counties')
     def counties(self, request, pk):
-        return Response(self.sparql_query(self.get_counties_query(pk)))
+        return Response(self.sparql_query(self.get_counties_query(pk), True))
 
 
 class CountyViewSet(SparQLViewSet):
     def get_detail_query(self, pk=None):
         query = '''
             PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX rdfschema:<http://www.w3.org/2000/01/rdf-schema#>
             PREFIX igeo:<http://rdf.insee.fr/def/geo#>
-            SELECT ?label ?value WHERE {{
-                    ?state rdf:type igeo:Departement .
-                    ?state igeo:nom ?name .
-                    ?state igeo:vivant true .
-                    ?state igeo:codeINSEE ?insee_code .
-                    ?state ?prop ?value .
-                    ?prop <http://www.w3.org/2000/01/rdf-schema#label> ?label
+            SELECT ?prop ?value WHERE {{
+                    ?county rdf:type igeo:Departement .
+                    ?county igeo:nom ?name .
+                    ?county igeo:vivant true .
+                    ?county igeo:codeINSEE ?insee_code .
+                    ?prop rdfschema:label ?label .
+                    ?county ?prop ?value
                     FILTER( "{}" = STR(?insee_code)) .
+                    FILTER( isLiteral(?value) )
             }}
             '''
         return query.format(pk)
@@ -111,11 +141,11 @@ class CountyViewSet(SparQLViewSet):
         return '''
             PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
             PREFIX igeo:<http://rdf.insee.fr/def/geo#>
-            SELECT ?name ?insee_code WHERE {
-                ?state rdf:type igeo:Departement .
-                    ?state igeo:nom ?name .
-                    ?state igeo:vivant true .
-                    ?state igeo:codeINSEE ?insee_code .
+            SELECT ?identifier ?name ?insee_code WHERE {
+                    ?identifier rdf:type igeo:Departement .
+                    ?identifier igeo:nom ?name .
+                    ?identifier igeo:vivant true .
+                    ?identifier igeo:codeINSEE ?insee_code .
             }
             '''
 
@@ -123,12 +153,12 @@ class CountyViewSet(SparQLViewSet):
         query = '''
             PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
             PREFIX igeo:<http://rdf.insee.fr/def/geo#>
-            SELECT ?code_insee ?name WHERE {{
-                    ?township_id <http://rdf.insee.fr/def/geo#subdivisionDe> ?county .
-                    ?township_id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://rdf.insee.fr/def/geo#Commune> .
-                    ?township_id <http://rdf.insee.fr/def/geo#nom> ?name .
-                    ?township_id <http://rdf.insee.fr/def/geo#codeINSEE> ?code_insee .
-                    ?county igeo:codeINSEE ?county_insee_code
+            SELECT ?identifier ?name ?insee_code WHERE {{
+                    ?identifier <http://rdf.insee.fr/def/geo#subdivisionDe> ?county .
+                    ?identifier <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://rdf.insee.fr/def/geo#Commune> .
+                    ?identifier <http://rdf.insee.fr/def/geo#nom> ?name .
+                    ?identifier <http://rdf.insee.fr/def/geo#codeINSEE> ?insee_code .
+                    ?state igeo:codeINSEE ?county_insee_code
                     FILTER( "{}" = STR(?county_insee_code))
             }}
         '''
@@ -136,14 +166,18 @@ class CountyViewSet(SparQLViewSet):
         
     @detail_route(url_path='townships')
     def townships(self, request, pk):
-        return Response(self.sparql_query(
-            '''
+        return Response(self.sparql_query(self.get_townships_query(pk), True))
+
+
+class TownshipViewset(SparQLViewSet):
+    def get_list_query(self):
+        return '''
             PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
             PREFIX igeo:<http://rdf.insee.fr/def/geo#>
-            SELECT ?a ?b WHERE {
-                <http://id.insee.fr/geo/mouvements/279> ?a ?b
+            SELECT ?identifier ?name ?insee_code WHERE {
+                    ?identifier rdf:type igeo:Commune .
+                    ?identifier igeo:nom ?name .
+                    ?identifier igeo:vivant true .
+                    ?identifier igeo:codeINSEE ?insee_code .
             }
-            ''')
-        )
-
-        return Response(self.sparql_query(self.get_townships_query(pk)))
+        '''
