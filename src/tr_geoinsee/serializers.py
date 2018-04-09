a0abea0624d182cbd05d7cfb492f58dd81967e38
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-04-09 10:45:46 +0200

    Sets SparQL query serializer

--- src/tr_geoinsee/serializers.py
+++ src/tr_geoinsee/serializers.py
@@ -1,5 +1,15 @@
-from rest_framework import serializers
+# coding: utf8
+from rest_framework import serializers, fields
 
 class StateSerializer(serializers.Serializer):
     insee_code = serializers.IntegerField()
     name = serializers.CharField()
+
+
+class SparQLSerializer(serializers.Serializer):
+    def get_fields(self):
+        '''
+        Set all fields as CharField based on first item of list in case of ListSerializer or instance item
+        '''
+        instance = self.instance[0] if type(self.instance) is list else self.instance
+        return { key: fields.CharField() for key in dict(instance).keys() } 
