63d350039a085907f51837ecc5fe3728733e5b73
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-04-18 15:56:48 +0200

    #12 - improve tests cover of utils.py

--- src/geoinsee/tests/test_api.py
+++ src/geoinsee/tests/test_api.py
@@ -1,5 +1,8 @@
+import logging 
+
 from django.urls import reverse
 
+logger = logging.getLogger(__name__)
 
 def test_state_returned_fields(client):
     """ Test State views returned fields  """
@@ -82,3 +85,32 @@ def test_township_returned_fields(client):
 
     assert response.status_code == 200
     assert list(response.data.keys()) == ['insee', 'name']
+
+def test_invalid_endpoint(client, settings):
+    settings.INSEE_API_URL = 'http://my.failing.url/with/endpoint/'
+
+    """
+    Test invalid endpoint response
+    """
+    response = client.get(reverse('state-list'))
+    assert response.status_code == 200
+    assert response.data.get('count') == 0
+    assert len(response.data.get('results')) == 0
+
+    """
+    Test pagination with invalid backend datas so it's and invalid
+    page number
+    """
+    response = client.get(reverse('state-list'), {'page': 2})
+    assert response.data.get('count') == 0
+    assert response.data.get('links').get('next') is None
+    assert response.data.get('links').get('previous') is None
+
+def test_page_two(client):
+    """
+    Test links header in page 2: previous and next pages must be 
+    present in this view
+    """
+    response = client.get(reverse('township-list'), {'page': 2})
+    assert response.data.get('links').get('next') is not None
+    assert response.data.get('links').get('previous') is not None
