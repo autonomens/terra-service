38fc659c0b615da72268c50815122464ad74bfe0
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-04-20 10:04:11 +0200

    #11 - fix import

--- src/geocoding/views.py
+++ src/geocoding/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
 
-from .geocodingapi import GeocodingFactory
+from .backend import GeocodingFactory
 
 class GeocodingViewset(viewsets.ViewSet):
 
