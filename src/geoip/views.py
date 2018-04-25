6e51a8216864b94fab912eab3bf18d9ca1c08e9d
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-04-25 11:32:52 +0200

    #11 - add exception message and check if ip content is not empty

--- src/geoip/views.py
+++ src/geoip/views.py
@@ -9,8 +9,8 @@ from rest_framework.response import Response
 
 class GeoIPView(APIView):
     def get(self, request, format=None):
-        if not 'ip' in request.GET:
-            return HttpResponseBadRequest()
+        if not 'ip' in request.GET or not request.GET.get('ip'):
+            return HttpResponseBadRequest("Missing IP parameter")
 
         geoip = GeoIP2()
         return Response(geoip.city(request.GET.get('ip')))
