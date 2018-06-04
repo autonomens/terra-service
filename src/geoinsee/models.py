74c6dbfcf706c50085178bedd4a6efde3b33bfd3
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2018-06-04 16:06:19 +0200

    Add administrative area type in AdministrativeEntity
    Make constraint unique together with insee and entity_type

--- src/geoinsee/models.py
+++ src/geoinsee/models.py
@@ -1,12 +1,35 @@
 from django.contrib.gis.db import models
 from django.conf import settings
+from django.utils.translation import gettext as _
 # Create your models here.
 
 
 class AdministrativeEntity(models.Model):
-    name = models.CharField(blank=False, max_length=163, help_text='Administrative Entity name')
-    insee = models.CharField(max_length=5, blank=False, unique=True, help_text='Insee Code')
+
+    ENTITY_TYPE_TOWN = 'township'
+    ENTITY_TYPE_SCOT = 'scot'
+    ENTITY_TYPE_EPCI = 'epci'
+    ENTITY_TYPE_COUNTY = 'county'
+    ENTITY_TYPE_REGION = 'region'
+    ENTITY_TYPE_STATE = 'state'
+    ENTITY_TYPE_COUNTRY = 'country'
+    ENTITY_TYPE_CHOICES = (
+        (ENTITY_TYPE_TOWN, _('Township')),
+        (ENTITY_TYPE_SCOT, _('SCOT')),
+        (ENTITY_TYPE_EPCI, _('EPCI')),
+        (ENTITY_TYPE_COUNTY, _('County')),
+        (ENTITY_TYPE_REGION, _('Region')),
+        (ENTITY_TYPE_STATE, _('State')), # US
+        (ENTITY_TYPE_COUNTRY, _('Country')),
+    )
+    name = models.CharField(blank=False, max_length=163, help_text=_('Administrative Entity name'))
+    insee = models.CharField(max_length=5, blank=False, help_text='Insee Code')
+    entity_type = models.CharField(null=False, default=None,
+        max_length=15, choices=ENTITY_TYPE_CHOICES, help_text=_('Administrative area type'))
     geom = models.GeometryField(dim=2, srid=settings.SRID, spatial_index=False, editable=False, null=False, default=None)
+
+    class Meta:
+        unique_together = ('insee', 'entity_type',)
 
 
 class ZIPCode(models.Model):
